75e69ad4f76210226e8a33b0f9262dabbfaa9378
Glitch (mature-mellow-session) <none>
2021-06-03 09:39:27 +0000

    👨‍❤️‍👨🎪 Checkpoint ./config.js:23490712/202

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/a9SHDpD", //Support Server Link
-  Token: process.env.Token || "", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", //Discord Client Secret
+  Token: process.env.Token || "ODQ5OTQxNDI1Njk2MDE0MzU2.YLifow.cvfLd5Npl4DKiB5mw4WlVQMY6Ps", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "849941425696014356", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "xMQ2V9CIbPKoqLs18PYDMQYjC_Vy2Cju", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
-  CookieSecret: "Pikachu is cute", //A Secret like a password
+  CookieSecret: "Pikachu fazla epic", //A Secret like a password
   IconURL:
     "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
   Permissions: 2205280576, //Bot Inviting Permissions
@@ -36,7 +36,7 @@ module.exports = {
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "", //Spotify Client Secret
+    ClientID: process.env.Spotify_ClientID || "50fe660b1b5f43c8b063a10f332b1b93", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "799998fe00f84e90928f4d1aa21c877d", //Spotify Client Secret
   },
 };
