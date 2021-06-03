b5fb488f9ff3aa87ac9b7425fb099b6fe3bf2411
Glitch (mature-mellow-session) <none>
2021-06-03 09:41:27 +0000

    🚌💼 Checkpoint ./config.js:23490712/3906

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  Admins: ["UserID", "UserID"], //Admins of the bot
-  ExpressServer: true,//If you wanted to make the website run or not
+  Admins: ["602627713190068229", "713286920951693414"], //Admins of the bot
+  ExpressServer: true, //If you wanted to make the website run or not
   DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/a9SHDpD", //Support Server Link
-  Token: process.env.Token || "ODQ5OTQxNDI1Njk2MDE0MzU2.YLifow.cvfLd5Npl4DKiB5mw4WlVQMY6Ps", //Discord Bot Token
+  Token:
+    process.env.Token ||
+    "ODQ5OTQxNDI1Njk2MDE0MzU2.YLifow.cvfLd5Npl4DKiB5mw4WlVQMY6Ps", //Discord Bot Token
   ClientID: process.env.Discord_ClientID || "849941425696014356", //Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "xMQ2V9CIbPKoqLs18PYDMQYjC_Vy2Cju", //Discord Client Secret
+  ClientSecret:
+    process.env.Discord_ClientSecret || "xMQ2V9CIbPKoqLs18PYDMQYjC_Vy2Cju", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
@@ -17,13 +20,13 @@ module.exports = {
   Website: process.env.Website || "http://localhost", //Website where it was hosted at includes http or https || Use "0.0.0.0" if you using Heroku
 
   //Lavalink
-   Lavalink: {
+  Lavalink: {
     id: "Main",
     host: "lava.link",
     port: 80,
-    pass: "youshallnotpass", 
+    pass: "youshallnotpass"
   },
-  
+
   //Alternate Lavalink
   /*
   Lavalink: {
@@ -36,7 +39,9 @@ module.exports = {
 
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "50fe660b1b5f43c8b063a10f332b1b93", //Spotify Client ID
-    ClientSecret: process.env.Spotify_ClientSecret || "799998fe00f84e90928f4d1aa21c877d", //Spotify Client Secret
-  },
+    ClientID:
+      process.env.Spotify_ClientID || "50fe660b1b5f43c8b063a10f332b1b93", //Spotify Client ID
+    ClientSecret:
+      process.env.Spotify_ClientSecret || "799998fe00f84e90928f4d1aa21c877d" //Spotify Client Secret
+  }
 };
