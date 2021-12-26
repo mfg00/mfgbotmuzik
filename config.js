a7f6cc6cde1ed3847e67c3a776ad653ab82b2473
Glitch (mature-mellow-session) <none>
2021-12-26 14:00:43 +0000

    ⛈🌍 Checkpoint ./config.js:28723931/384 ./commands/bassboost.js:28723931/144 ./events/ready.js:28723931/21

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
   Admins: ["602627713190068229", "713286920951693414"], //Admins of the bot
   ExpressServer: true, //If you wanted to make the website run or not
-  DefaultPrefix: process.env.Prefix || ">", //Default prefix, Server Admins can change the prefix
+  DefaultPrefix: process.env.Prefix || ".", //Default prefix, Server Admins can change the prefix
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/a9SHDpD", //Support Server Link
   Token:
     process.env.Token ||
-    "ODQ5OTQxNDI1Njk2MDE0MzU2.YLifow.cvfLd5Npl4DKiB5mw4WlVQMY6Ps", //Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "849941425696014356", //Discord Client ID
+    "OTI0NjQyMTMyMzI3ODAwODM1.YchiFw.2p49VtjYLtiWWLHaWAw9brudvhk", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "924642132327800835", //Discord Client ID
   ClientSecret:
-    process.env.Discord_ClientSecret || "xMQ2V9CIbPKoqLs18PYDMQYjC_Vy2Cju", //Discord Client Secret
+    process.env.Discord_ClientSecret || "21f1W_JaNYlgDPdQ9tsMyKoA9yFO3xpg", //Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
   CallbackURL: "/api/callback", //Discord OAuth2 Callback URL
   "24/7": false, //If you want the bot to be stay in the vc 24/7
@@ -40,8 +40,8 @@ module.exports = {
   //Please go to https://developer.spotify.com/dashboard/
   Spotify: {
     ClientID:
-      process.env.Spotify_ClientID || "50fe660b1b5f43c8b063a10f332b1b93", //Spotify Client ID
+      process.env.Spotify_ClientID || "75d2ec3fa90b4c549a8e559e6944bff5", //Spotify Client ID
     ClientSecret:
-      process.env.Spotify_ClientSecret || "799998fe00f84e90928f4d1aa21c877d" //Spotify Client Secret
+      process.env.Spotify_ClientSecret || "37d3736014584a22b3fa26aefa8bbb50" //Spotify Client Secret
   }
 };
